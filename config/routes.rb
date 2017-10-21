a7c18304fe9877e7c4fbc9c1ea1378adfb4596c3
Devroy Blake <necrodevz@gmail.com>
2017-10-21 22:25:00 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
-  get 'pages/index'
+  root 'pages#index'
+  get '/*pages', to: 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
